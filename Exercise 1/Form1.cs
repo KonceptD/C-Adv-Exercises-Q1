59ff474d948ff9dd1ff1c981518dec11d888eeee
KonceptD <kiranjayd@yahoo.com>
2023-11-15 17:54:26 +0200

    Added comments to code

--- Exercise 1/Form1.cs
+++ Exercise 1/Form1.cs
@@ -7,54 +7,81 @@ namespace Exercise_1
     {
 
         public Form1()
-        {
+        {   
+            // loads the form
             InitializeComponent();
+
+            // Create a new arrayList for the ListBox
             var arlist = new ArrayList()
             {
+                // adding elements using object initializer syntax rather than populate it one by one
                 "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
             };
             foreach (var ar in arlist)
             {
+                // For each item in the arrayList, add it to the ListBox Items collection
                 listBox.Items.Add(ar);
             };
-            listBox.SelectedIndexChanged += new EventHandler(listBox_SelectedIndexChanged);
-            // Here, every time a different item on the listbox is clicked, we are are saying to trigger an "new" event handler to accomodate that change
-            // in this case, referring the "listBox_SelectedIndexChanged" event handler at the bottom of this code.
+
+            // Here, every time a different item on the listbox is clicked (SelectedIndexChanged), we are are saying to trigger an "new" event handler to accomodate that change
+            listBox.SelectedIndexChanged += new EventHandler(listBox_SelectedIndexChanged); // in this case, referring the "listBox_SelectedIndexChanged" event handler at the bottom of this code.
+
             // += means "Add this event handler to the list of handlers that should be called when the SelectedIndexChanged event is fired."
 
         }
 
-        private void btnsave_Click(object sender, EventArgs e)
+        // Saving the edited or newly added text to file
+        private void btnsave_Click(object sender, EventArgs e) 
         {
+            // the selectDay var takes the selected item once it has been converted to a string
             string selectedDay = listBox.SelectedItem.ToString();
+
+            // Assign the file path of the text file using string interpolation so we don't have to do it manually for each day
             string filePath = $"C:/Users/kiran/Downloads/My Week Days/{selectedDay}.txt";
+
+            // Practice to catch errors, ensuring the success
             try
             {
+                // assign the new streamwriter instance and filepath to a var
                 StreamWriter sw = new StreamWriter(filePath);
+
+                // Streamwriter writers the new or edited info in from the textbox text 
                 sw.WriteLine(textBox.Text);
-                sw.Close();
+
+                // Close the StreamWrite stream
+                sw.Close(); 
             }
             catch (Exception ex)
             {
-
-                MessageBox.Show($"Error writing file: {ex.Message}");
+                // If the streamwriting fails, a message box will pop up
+                MessageBox.Show($"Error writing file: {ex.Message}"); 
             }
         }
 
-        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        // Event handler for when the selected item in the listbox changes
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e) 
         {
+            // the selectDay var takes the selected item once it has been converted to a string
             string selectedDay = listBox.SelectedItem.ToString();
+
+            // Assign the file path of the text file using string interpolation so we don't have to do it manually for each day
             string filePath = $"C:/Users/kiran/Downloads/My Week Days/{selectedDay}.txt";
 
+            // Practice to catch errors, ensuring the success
             try
             {
+                // Assign the new StreamReader instance and filepath to a var
                 StreamReader sr = new StreamReader(filePath);
+
+                // Populates the TextBox text with StreamRead file once its read to the end
                 textBox.Text = sr.ReadToEnd();
+
+                // Closes the streamRead stream
                 sr.Close();
             }
             catch (Exception ex)
             {
-
+                // If the streamwriting fails, a message box will pop up
                 MessageBox.Show($"Error reading file: {ex.Message}");
             }
         }
